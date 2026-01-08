19fee36c69bfecc71fdaa294700830391fc9ad71
prince33805 <prince30887@gmail.com>
2026-01-08 14:23:53 +0700

    fix: specify host for app.listen in bootstrap function

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
       credentials: true,
     });
 
-    await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
   }
   catch (err) {
     console.error("❌ Bootstrap failed:", err);
